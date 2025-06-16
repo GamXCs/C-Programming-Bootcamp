c0f76002dc6bc21f40f7ca13b567df36c87a19a8
Walter Stewart <gavlevi@uab.edu>
2025-06-15 23:08:30 -0500

    Commit before merge

--- main.c
+++ main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-//getting input from the user
-//the '&' refers to the memory address
-    int grade1;
-    int grade2;
-    scanf("%d",&grade1);
-    scanf("%d",&grade2);
-    printf("Average=%d\n",(grade1+grade2)/2);
+// getting input from the user
+// the '&' refers to the memory address
+     int grade1;
+     int grade2;
+     scanf("%d",&grade1);
+     scanf("%d",&grade2);
+     printf("Average=%d\n",(grade1+grade2)/2);
+
 
     return 0;
 }
